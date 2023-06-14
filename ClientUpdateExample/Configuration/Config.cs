8b3106a41bf8eded057084ccc0fb65e25d97b76a
Kåre <kare.brekke@nhn.no>
2023-06-15 00:16:40 +0200

    Client update example.

---
+++ ClientUpdateExample/Configuration/Config.cs
@@ -0,0 +1,33 @@
+﻿namespace ClientUpdateExample.Configuration;
+
+internal class Config
+{
+    public required HelseIdConfig HelseId { get; set; }
+    public required SelvbetjeningConfig Selvbetjening { get; set; }
+    public required ClientConfig Client { get; set; }
+}
+
+internal class HelseIdConfig
+{
+    public required string Authority { get; set; }
+}
+
+internal class SelvbetjeningConfig
+{
+    public required string ApiUri { get; set; }
+    public required string ClientStatusEndpoint { get; set; }
+    public required string ClientSecretEndpoint { get; set; }
+    public required string ClientEndpoint { get; set; }
+
+    public string ClientUri => GetEndpointUri(ClientEndpoint);
+    public string ClientStatusUri => GetEndpointUri(ClientStatusEndpoint);
+    public string ClientSecretUri => GetEndpointUri(ClientSecretEndpoint);
+
+    private string GetEndpointUri(string endpointPath) => new Uri(new Uri(ApiUri), endpointPath).ToString();
+}
+
+internal class ClientConfig
+{
+    public required string ClientId { get; set; }
+    public required string Jwk { get; set; }
+}
---
+++ ClientUpdateExample/Program.cs
@@ -0,0 +1,117 @@
+﻿using Auth;
+using Auth.Utils;
+using ClientUpdateExample.Configuration;
+using Common.Crypto;
+using Common.Models;
+using Common.Models.Response;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+internal static class Program
+{
+    private const string SelvbetjeningClientScope = "nhn:selvbetjening/client";
+
+    private static async Task Main()
+    {
+        var config = GetConfig();
+
+        using var authHttpClient = new AuthHttpClient();
+
+        var update = new ClientUpdate
+        {
+            ApiScopes = new[] { SelvbetjeningClientScope, "nhn:kjernejournal/api" },
+            AudienceSpecificClientClaims = null,
+            ChildOrganizationNumbers = null,
+            PostLogoutRedirectUris = null,
+            RedirectUris = null,
+        };
+
+        try
+        {
+            await UpdateClient(authHttpClient, config, update);
+            await Out("Update complete");
+        }
+        catch (Exception ex)
+        {
+            await Out($"Update failed: {ex.Message}");
+        }
+
+        //await UpdateClientSecret(config, authHttpClient);
+    }
+
+    private static async Task UpdateClient(AuthHttpClient authHttpClient, Config config, ClientUpdate update)
+    {
+        string accessToken = await GetAccessToken(config);
+
+        await authHttpClient.Put<ClientUpdate>(config.Selvbetjening.ClientUri, update, accessToken: accessToken);
+    }
+
+    private static async Task UpdateClientSecret(Config config, AuthHttpClient authHttpClient)
+    {
+        var newJwk = KeyGenerator.GenerateJwk();
+
+        var clientSecretUpdateResponse = await UpdateClientSecret(authHttpClient, config, newJwk.publicJwk);
+
+        await Out($"New client secret expiration: {clientSecretUpdateResponse.Expiration}");
+    }
+
+    private static async Task<ClientSecretUpdateResponse> UpdateClientSecret(AuthHttpClient authHttpClient, Config config, string newPublicJwk)
+    {
+        string accessToken = await GetAccessToken(config);
+
+        return await authHttpClient.Post<string, ClientSecretUpdateResponse>(config.Selvbetjening.ClientSecretUri, newPublicJwk, accessToken: accessToken);
+    }
+
+    private static async Task<string> GetAccessToken(Config config)
+    {
+        var clientCredentialsTokens = await GetClientCredentialsTokens(config.HelseId.Authority, config.Client.ClientId, config.Client.Jwk, new[] { SelvbetjeningClientScope });
+
+        return clientCredentialsTokens.AccessToken;
+    }
+
+    private static async Task<Tokens> GetClientCredentialsTokens(string authority, string clientId, string publicAndPrivateJwk, string[] scopes)
+    {
+        using var auth = new SystemAuthenticator(authority);
+
+        return await auth.GetTokens(clientId, publicAndPrivateJwk, scopes);
+    }
+
+    private static async Task Out(string message)
+    {
+        await Console.Out.WriteLineAsync(message);
+    }
+
+    private static Config GetConfig()
+    {
+        var builder = new ConfigurationBuilder()
+                              .SetBasePath(Directory.GetCurrentDirectory())
+                              .AddJsonFile("appsettings.json", optional: false)
+                              .AddJsonFile("appsettings.Local.json", optional: true);
+
+        IConfiguration configuration = builder.Build();
+
+        var config = configuration.Get<Config>();
+
+        if (config == null)
+        {
+            throw new Exception("Config is null.");
+        }
+
+        return config;
+    }
+
+    static Program()
+    {
+        _jsonSerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+        {
+            WriteIndented = false,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+            IgnoreReadOnlyProperties = true,
+        };
+
+        _jsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    }
+
+    private static readonly JsonSerializerOptions _jsonSerializerOptions;
+}
