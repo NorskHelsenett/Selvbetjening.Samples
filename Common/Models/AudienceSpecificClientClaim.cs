6d238c59eda4d496494d7856f630ba96d9f95eef
Magnus Bjerke Vik <magnus.bjerke.vik@conceptos.no>
2023-06-22 13:40:13 +0200

    Fix ASCC for ClientUpdate not being serialized correctly

--- Common/Models/AudienceSpecificClientClaim.cs
+++ Common/Models/AudienceSpecificClientClaim.cs
@@ -1,7 +1,3 @@
 namespace Common.Models;
 
-public class AudienceSpecificClientClaim
-{
-    public required string Key { get; set; }
-    public required string Value { get; set; }
-}
+public record AudienceSpecificClientClaim(string Key, string Value);
--- Common/Models/ClientUpdate.cs
+++ Common/Models/ClientUpdate.cs
@@ -3,7 +3,7 @@
     public class ClientUpdate
     {
         public string[] ApiScopes { get; set; } = Array.Empty<string>();
-        public KeyValuePair<string, string>[]? AudienceSpecificClientClaims { get; set; }
+        public AudienceSpecificClientClaim[]? AudienceSpecificClientClaims { get; set; }
         public string[]? RedirectUris { get; set; }
         public string[]? PostLogoutRedirectUris { get; set; }
         public string[]? ChildOrganizationNumbers { get; set; }
