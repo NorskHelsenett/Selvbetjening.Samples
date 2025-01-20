dfdafc5310a0972e309bb3de7bdb16e872876644
Magnus Bjerke Vik <magnus.bjerke.vik@nhn.no>
2025-01-20 16:29:35 +0100

    Simplify and clean up some things

--- Auth/Utils/AuthHttpClient.cs
+++ Auth/Utils/AuthHttpClient.cs
@@ -1,7 +1,6 @@
 ﻿using Common.Models;
 using IdentityModel.Client;
-using System.Net.Http.Headers;
-using System.Text;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -12,71 +11,37 @@ public class AuthHttpClient : IDisposable
     private readonly HttpClient _httpClient;
     private readonly JwkWithMetadata? _dPoPKey;
 
-    public AuthHttpClient(JwkWithMetadata? dPoPKey = null, bool logHttp = false)
+    public AuthHttpClient(JwkWithMetadata? dPoPKey = null)
     {
-        _httpClient = logHttp
-            ? new HttpClient(new RawHttpLoggingHandler(new HttpClientHandler()))
-            : new HttpClient();
+        _httpClient = new HttpClient();
         _dPoPKey = dPoPKey;
     }
 
-    public async Task<T> Get<T>(string uri, string? accessToken = null, IDictionary<string, string>? headers = null)
+    public async Task<TResponse> Get<TResponse>(string uri, string? accessToken = null, IDictionary<string, string>? headers = null)
     {
-        string json = await Get(uri, accessToken, headers);
+        var requestMessage = CreateRequestMessage(uri, accessToken, headers, HttpMethod.Get);
+        var response = await _httpClient.SendAsync(requestMessage);
 
-        return Deserialize<T>(json);
+        return await GetResponseMessage<TResponse>(response);
     }
 
-    public async Task<TResponseType> Post<TRequestType, TResponseType>(string uri, TRequestType body, string? accessToken = null, IDictionary<string, string>? headers = null)
+    public async Task<TResponse> Post<TRequest, TResponse>(string uri, TRequest body, string? accessToken = null, IDictionary<string, string>? headers = null)
     {
-        string jsonBody = body is string str ? str : JsonSerializer.Serialize(body, JsonSerializerOptions);
-        string jsonResponse = await Post(uri, jsonBody, accessToken, headers);
-
-        return Deserialize<TResponseType>(jsonResponse);
-    }
-
-    public async Task<TResponseType> Put<TRequestType, TResponseType>(string uri, TRequestType body, string? accessToken = null, IDictionary<string, string>? headers = null)
-    {
-        string jsonBody = JsonSerializer.Serialize(body, JsonSerializerOptions);
-        string jsonResponse = await Put(uri, jsonBody, accessToken, headers);
-
-        return Deserialize<TResponseType>(jsonResponse);
-    }
-
-    public async Task Put<TRequestType>(string uri, TRequestType body, string? accessToken = null, IDictionary<string, string>? headers = null)
-    {
-        string jsonBody = JsonSerializer.Serialize(body, JsonSerializerOptions);
-        var _ = await Put(uri, jsonBody, accessToken, headers);
-    }
-
-    public async Task<string> Get(string uri, string? accessToken = null, IDictionary<string, string>? headers = null)
-    {
-        using var requestMessage = CreateRequestMessage(uri, accessToken, headers, HttpMethod.Get);
+        var requestMessage = CreateRequestMessage(uri, accessToken, headers, HttpMethod.Post);
+        SetJsonContent(requestMessage, body);
 
         var response = await _httpClient.SendAsync(requestMessage);
 
-        return await GetResponseMessage(response);
+        return await GetResponseMessage<TResponse>(response);
     }
 
-    public async Task<string> Post(string uri, string body, string? accessToken = null, IDictionary<string, string>? headers = null)
+    public async Task Put<TRequest>(string uri, TRequest body, string? accessToken = null, IDictionary<string, string>? headers = null)
     {
-        return await SendWithContent(uri, body, accessToken, headers, HttpMethod.Post);
-    }
-
-    public async Task<string> Put(string uri, string body, string? accessToken = null, IDictionary<string, string>? headers = null)
-    {
-        return await SendWithContent(uri, body, accessToken, headers, HttpMethod.Put);
-    }
-
-    private async Task<string> SendWithContent(string uri, string body, string? accessToken, IDictionary<string, string>? headers, HttpMethod method)
-    {
-        var requestMessage = CreateRequestMessage(uri, accessToken, headers, method);
-
-        requestMessage.Content = new StringContent(body, Encoding.UTF8, MediaTypeHeaderValue.Parse("application/json"));
+        var requestMessage = CreateRequestMessage(uri, accessToken, headers, HttpMethod.Put);
+        SetJsonContent(requestMessage, body);
 
         var response = await _httpClient.SendAsync(requestMessage);
-
-        return await GetResponseMessage(response);
+        await EnsureSuccess(response);
     }
 
     private HttpRequestMessage CreateRequestMessage(string uri, string? accessToken, IDictionary<string, string>? headers, HttpMethod method)
@@ -106,28 +71,31 @@ public class AuthHttpClient : IDisposable
         return requestMessage;
     }
 
-    private static async Task<string> GetResponseMessage(HttpResponseMessage response)
+    private void SetJsonContent<TRequest>(HttpRequestMessage requestMessage, TRequest body)
     {
-        string responseMessage = await response.Content.ReadAsStringAsync();
-
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new Exception($"Http request failed. Code: {response.StatusCode} Content: {responseMessage}");
-        }
-
-        return responseMessage;
+        requestMessage.Content = JsonContent.Create(body, options: JsonSerializerOptions);
     }
 
-    private static T Deserialize<T>(string json)
+    private static async Task<TResponse> GetResponseMessage<TResponse>(HttpResponseMessage response)
     {
-        var deserialized = JsonSerializer.Deserialize<T>(json, options: JsonSerializerOptions);
+        await EnsureSuccess(response);
 
-        if (deserialized == null)
+        var responseBody = await response.Content.ReadFromJsonAsync<TResponse>(options: JsonSerializerOptions);
+        if (responseBody == null)
         {
-            throw new Exception($"{json} was deserialized as null.");
+            throw new Exception("Response contained null value");
         }
 
-        return deserialized;
+        return responseBody;
+    }
+
+    private static async Task EnsureSuccess(HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode)
+        {
+            var responseMessage = await response.Content.ReadAsStringAsync();
+            throw new Exception($"Http request failed. Code: {response.StatusCode} Content: {responseMessage}");
+        }
     }
 
     public void Dispose()
