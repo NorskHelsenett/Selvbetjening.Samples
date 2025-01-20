5f1e845bb2ad252fad5a2e0e689444f1667d0283
Magnus Bjerke Vik <magnus.bjerke.vik@nhn.no>
2025-01-20 16:29:06 +0100

    Remove HelseIdJwtBuilder and support for assert/authorization details

--- Auth/Utils/ClientAssertionBuilder.cs
+++ Auth/Utils/ClientAssertionBuilder.cs
@@ -1,18 +1,52 @@
-﻿using IdentityModel;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using IdentityModel;
 using IdentityModel.Client;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Auth.Utils;
 
 public static class ClientAssertionBuilder
 {
-    public static ClientAssertion Build(string clientId, string jwk, string authority, string[]? assertionDetails = null)
+    public static ClientAssertion Build(string clientId, string jwk, string authority)
     {
-        var clientAssertionString = HelseIdJwtBuilder.Build(clientId, jwk, authority, assertionDetails: assertionDetails);
+        var claims = new List<Claim>
+        {
+            new(JwtClaimTypes.Subject, clientId),
+            new(JwtClaimTypes.IssuedAt, DateTimeOffset.Now.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64),
+            new(JwtClaimTypes.JwtId, Guid.NewGuid().ToString("N")),
+        };
+
+        var credentials =
+            new JwtSecurityToken(
+                clientId,
+                authority,
+                claims,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddSeconds(60),
+                GetClientAssertionSigningCredentials(jwk));
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var clientAssertionString = tokenHandler.WriteToken(credentials);
 
         return new ClientAssertion
         {
             Type = OidcConstants.ClientAssertionTypes.JwtBearer,
             Value = clientAssertionString
         };
+    }
+
+    private static SigningCredentials GetClientAssertionSigningCredentials(string jwk)
+    {
+        var securityKey = new JsonWebKey(jwk);
+
+        var alg = securityKey.Alg;
+        if (string.IsNullOrWhiteSpace(alg))
+        {
+            throw new Exception("JWK must include the 'alg' parameter");
+        }
+
+        return new SigningCredentials(securityKey, alg);
     }
 }
